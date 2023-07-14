90f67590d3abfb8693b30104ea88bd127e12e9b4
Ilona <dobro.ilona@yandex.ru>
2023-07-14 19:37:05 +0300

    Initial Commit

---
+++ src/main/java/TicketTimeComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        if (t1.timeFlight() < t2.timeFlight()) {
+            return -1;
+        } else if (t1.timeFlight() > t2.timeFlight()) {
+            return 1;
+        }
+        return 0;
+    }
+}
